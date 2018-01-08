7304da2490b53b3a568d42615aafa2bc8c99aabe
王冬 <20004604@qq.com>
2018-01-08 11:35:57 +0800

    第一次更新，更新了时间段分析

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 王冬 on 2018/1/8.
+ * QQ: 20004604
+ * weChat: qq20004604
+ */
+
+function getTimingInfo() {
+    let timing = performance.timing
+    if (!timing) {
+        return null
+    }
+}
